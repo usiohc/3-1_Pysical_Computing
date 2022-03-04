1fafb42301542ec0ee49532760daf10c82fadd51
usiohc <asltn99@gmail.com>
2022-03-04 19:29:37 +0900

    week1 ver1.1

---
+++ Phyical/week1/list_001.py
@@ -0,0 +1,11 @@
+# 리스트
+# 리스트는 특정 순서가 있는 항목의 모음
+# 대괄호 [ ] 쉼표 기호를 사용함
+
+empty_list = []
+print(type(empty_list)) # result-><class 'list'>
+# type() -> 자료형의 타입 출력
+print(type(5))
+print(type(5.7))
+print(type('python'))
+
